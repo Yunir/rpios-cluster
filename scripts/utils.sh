e119c08dc8a8728d710ee7de91c7bfdc09db0c63
Yunir Salimzyanov <yunir.me@gmail.com>
2022-01-20 13:12:13 +0500

    Implement Kubernetes deployment via Ansible

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# run after burning iso image to micro-sd card
-pushd ../cloud-init
-cp * /Volumes/system-boot/
-popd
-
-diskutil umount /Volumes/system-boot
-
-
-# on rpios server node
-sudo echo "net.ifnames=0 dwc_otg.lpm_enable=0 console=serial0,115200 cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 console=tty1 root=LABEL=writable rootfstype=ext4 elevator=deadline rootwait fixrtc" > /boot/firmware/cmdline.txt
-curl -sfL https://get.k3s.io | sh -
-
-# for agent node
-sudo echo "net.ifnames=0 dwc_otg.lpm_enable=0 console=serial0,115200 cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 console=tty1 root=LABEL=writable rootfstype=ext4 elevator=deadline rootwait fixrtc" > /boot/firmware/cmdline.txt
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.0.211:6443 K3S_TOKEN=K10aca3879dbf9e02b43bb9cea51828b904889de3c7d399db5ed638f68415a99424::server:c0dca1addf14cf837fcdc19e25ba023a sh -
-
-
 # dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
 #  namespace/kubernetes-dashboard created
